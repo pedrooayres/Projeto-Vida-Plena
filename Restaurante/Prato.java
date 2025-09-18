65ecb432c5c5769937d8c234aa0e01022fdfd2ad
pedrooayres <pedrohayres@yahoo.com.br>
2025-09-18 16:42:58 +0000

    upgrade em prato.java - categoria da comida + calorias

--- Restaurante/Prato.java
+++ Restaurante/Prato.java
@@ -3,17 +3,23 @@ package Restaurante;
 public class Prato {
     private String nome;
     private double preco;
+    private String categoria;
+    private int calorias;
 
-    public Prato(String nome, double preco) {
+    public Prato(String nome, double preco, String categoria, int calorias) {
         this.nome = nome;
         this.preco = preco;
+        this.categoria = categoria;
+        this.calorias = calorias;
     }
 
     public String getNome() { return nome; }
     public double getPreco() { return preco; }
+    public String getCategoria() { return categoria; }
+    public int getCalorias() { return calorias; }
 
     @Override
     public String toString() {
-        return nome + " - R$" + preco;
+        return nome + " (" + categoria + ") - R$" + preco + " - " + calorias + " kcal";
     }
 }
