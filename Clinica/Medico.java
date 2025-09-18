d88fddcb6e070a932900535163310ed2b2f2118c
pedrooayres <pedrohayres@yahoo.com.br>
2025-09-18 16:41:41 +0000

    solicita o crm do médico

--- Clinica/Medico.java
+++ Clinica/Medico.java
@@ -4,16 +4,19 @@ import Base.Pessoa;
 
 public class Medico extends Pessoa {
     private String especialidade;
+    private String crm;
 
-    public Medico(String nome, String cpf, String contato, String especialidade) {
+    public Medico(String nome, String cpf, String contato, String especialidade, String crm) {
         super(nome, cpf, contato);
         this.especialidade = especialidade;
+        this.crm = crm;
     }
 
     public String getEspecialidade() { return especialidade; }
+    public String getCrm() { return crm; }
 
     @Override
     public String toString() {
-        return "Dr(a). " + getNome() + " - " + especialidade;
+        return "Dr(a). " + getNome() + " - " + especialidade + " (CRM: " + crm + ")";
     }
 }
