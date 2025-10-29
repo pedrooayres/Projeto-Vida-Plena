aef5d613d0a2cc25e2f2f5588f42995e15f4c831
pedrooayres <pedrohayres@yahoo.com.br>
2025-10-29 15:04:21 -0300

    Update EventosRelatorios.java
    versão nova

--- Eventos/EventosRelatorios.java
+++ Eventos/EventosRelatorios.java
@@ -14,12 +14,6 @@ public class EventosRelatorios {
                 .max(Comparator.comparingInt(Evento::getTotalInscritos))
                 .orElse(null);
     }
-     
-    /* metodos respostas - eventos
-    1- horarios de ocupação para cada evento, 2 - dia com mais falta, 3- evento com mais participantes
-    4- dia da com maior concentração de eventos, 5- renda mensal, 6- qual faixa de horario mais movimentada
-    7- gasto de cada pessoa, 8-qual data com maior numero de atividade, 9- percentual de presença
-    */
     public static void eventoComMaisFaltas(List <Evento> eventos){
         System.out.println("Dias com maior taxa de falta no eventos :");
     }
