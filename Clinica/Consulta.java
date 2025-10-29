0277e0b9bc7a114deeec0143b7aa3dba0de880b7
Diogo Carvalho <150797053+Dioguitin@users.noreply.github.com>
2025-10-29 16:05:09 -0300

    Update Consulta.java

--- Clinica/Consulta.java
+++ Clinica/Consulta.java
@@ -5,21 +5,21 @@ import Base.Agenda;
 public class Consulta {
     private Paciente paciente;
     private Medico medico;
-    private Agenda agenda;
-    private double valor;
+    private Agenda agenda_consulta;
+    private double valor_consulta;
     private int comparecimento_consulta;
 
-    public Consulta(Paciente paciente, Medico medico, Agenda agenda, double valor, int comparecimento_consulta) {
+    public Consulta(Paciente paciente, Medico medico, Agenda agenda_consulta, double valor_consulta, int comparecimento_consulta) {
         this.paciente = paciente;
         this.medico = medico;
-        this.agenda = agenda;
-        this.valor = valor;
+        this.agenda_consulta = agenda_consulta;
+        this.valor_consulta = valor_consulta;
         this.comparecimento_consulta = comparecimento_consulta;
     }
 
     public Medico getMedico() { return medico; }
     public Paciente getPaciente() { return paciente; }
-    public Agenda getAgenda() { return agenda; }
-    public double getValor() { return valor; }
+    public Agenda getAgenda() { return agenda_consulta; }
+    public double getValor() { return valor_consulta; }
     public int getComparecimento_Consulta() {return comparecimento_consulta; }
 }
