ae1c27f50057a940f9d21a026e3378f88196fbc2
Diogo Carvalho <150797053+Dioguitin@users.noreply.github.com>
2025-10-29 16:00:22 -0300

    Update Pedido.java

--- Restaurante/Pedido.java
+++ Restaurante/Pedido.java
@@ -3,14 +3,19 @@ package Restaurante;
 import java.util.ArrayList;
 import java.util.List;
 
+import Base.Agenda;
+
 public class Pedido {
     private List<Prato> itens = new ArrayList<>();
+    private Agenda agenda_pedido;
 
-    public void adicionarPrato(Prato prato) {
+    public void adicionarPrato(Prato prato, Agenda agenda_pedido) {
         itens.add(prato);
+        this.agenda_pedido = agenda_pedido;
     }
 
     public List<Prato> getItens() { return itens; }
+    public Agenda getAgenda() { return agenda_pedido; }
 
     public double calcularTotal() {
         return itens.stream().mapToDouble(Prato::getPreco).sum();
