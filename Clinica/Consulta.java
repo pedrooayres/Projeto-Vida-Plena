0c5589dfd16c93eee2e6c5fde8c70a92496e395c
pedrooayres <pedrohayres@yahoo.com.br>
2025-10-29 15:02:38 -0300

    Update Consulta.java
    versão mais recente

--- Clinica/Consulta.java
+++ Clinica/Consulta.java
@@ -7,16 +7,19 @@ public class Consulta {
     private Medico medico;
     private Agenda agenda;
     private double valor;
+    private int comparecimento_consulta;
 
-    public Consulta(Paciente paciente, Medico medico, Agenda agenda, double valor) {
+    public Consulta(Paciente paciente, Medico medico, Agenda agenda, double valor, int comparecimento_consulta) {
         this.paciente = paciente;
         this.medico = medico;
         this.agenda = agenda;
         this.valor = valor;
+        this.comparecimento_consulta = comparecimento_consulta;
     }
 
     public Medico getMedico() { return medico; }
     public Paciente getPaciente() { return paciente; }
     public Agenda getAgenda() { return agenda; }
     public double getValor() { return valor; }
+    public int getComparecimento_Consulta() {return comparecimento_consulta; }
 }
