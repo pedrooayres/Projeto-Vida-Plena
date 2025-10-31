c296e2a5aa8d48e8eb2f482ab00bfcd532a52995
Diogo Carvalho <150797053+Dioguitin@users.noreply.github.com>
2025-10-30 22:52:57 -0300

    Update ClinicaRelatorios.java

--- Clinica/ClinicaRelatorios.java
+++ Clinica/ClinicaRelatorios.java
@@ -19,20 +19,6 @@ public class ClinicaRelatorios {
                 .map(Map.Entry::getKey)
                 .orElse(null);
     }
-
-    public static String horarioComMaisFaltas(List<Consulta> consultas) {
-        Map<String, Integer> faltas = new HashMap<>();
-        for (Consulta c : consultas) {
-            if ("Falta".equalsIgnoreCase(c.getAgenda().getStatus())) {
-                String hora = c.getAgenda().getDataHora().getHour() + "h";
-                faltas.put(hora, faltas.getOrDefault(hora, 0) + 1);
-            }
-        }
-        return faltas.entrySet().stream()
-                .max(Map.Entry.comparingByValue())
-                .map(Map.Entry::getKey)
-                .orElse("Nenhum registro de faltas");
-    } 
     /* metodos respostas - clinica
      * perguntas 
      * 1 - horarios de ocupação para cada medico, 2- dia com mais falta, 3- medico mais requisito
