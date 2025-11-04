230866b50dd0a21ad2b22411c2ea2151aaff298b
Diogo Carvalho <150797053+Dioguitin@users.noreply.github.com>
2025-11-03 23:38:11 -0300

    RestauranteRelatorios.java atual

--- Restaurante/RestauranteRelatorios.java
+++ Restaurante/RestauranteRelatorios.java
@@ -5,23 +5,23 @@ import java.util.*;
 
 public class RestauranteRelatorios {
 
-    // ================== RELATÓRIO: PRATO MAIS VENDIDO ==================
     public static Prato pratoMaisVendidoPorPeriodo(List<Pedido> pedidos, String periodo) {
         Map<Prato, Integer> contador = new HashMap<>();
-
         for (Pedido p : pedidos) {
             for (Prato prato : p.getItens()) {
                 contador.put(prato, contador.getOrDefault(prato, 0) + 1);
             }
         }
-
         return contador.entrySet().stream()
                 .max(Map.Entry.comparingByValue())
                 .map(Map.Entry::getKey)
                 .orElse(null);
     }
-
-    // ================== RELATÓRIO: CLIENTE QUE MAIS CONSUMIU ==================
+     
+    /* metodos respostas - restuarante
+    4- dia da com maior concentração de eventos, 5-  renda mensal, 6- qual faixa de horario mais movimentada
+    7- gasto de cada pessoa,  8-qual data com maior numero de atividade, 9- percentual de presença
+    */
     public static Pessoa clienteMaisConsumiu(Map<Pessoa, List<Pedido>> pedidosPorCliente) {
         return pedidosPorCliente.entrySet().stream()
                 .max(Comparator.comparingDouble(e -> e.getValue().stream()
@@ -29,9 +29,7 @@ public class RestauranteRelatorios {
                 .map(Map.Entry::getKey)
                 .orElse(null);
     }
-
-    // ================== RELATÓRIO: FAIXA DE HORÁRIO COM MAIS CLIENTES ==================
-    public static void faixaComMaisClientes(List<Pedido> pedidos) {
-        System.out.println("Faixa de hora com mais clientes (ainda não implementada).");
+    public static void faixaComMaisClientes(List <Pedido> pedidos ){
+        System.out.println("Faixa de hora com mais clientes");
     }
 }
