e6185728b9e25da0de0b4ce4c3adfb9c1f38e76d
pedrooayres <pedrohayres@yahoo.com.br>
2025-11-05 15:46:31 -0300

    Add horario field and update Consulta constructor

--- Clinica/Consulta.java
+++ Clinica/Consulta.java
@@ -1,5 +1,7 @@
 package Clinica;
 
+import java.time.LocalDateTime;
+
 import Base.Agenda;
 
 public class Consulta {
@@ -8,18 +10,20 @@ public class Consulta {
     public Agenda agenda_consulta;
     private double valor_consulta;
     public boolean comparecimento_consulta;
+    public LocalDateTime horario;
 
-    public Consulta(Paciente paciente, Medico medico, Agenda agenda_consulta, double valor_consulta, boolean comparecimento_consulta) {
+    public Consulta(Paciente paciente, Medico medico, Agenda agenda_consulta, double valor_consulta, boolean comparecimento_consulta,LocalDateTime horario) {
         this.paciente = paciente;
         this.medico = medico;
         this.agenda_consulta = agenda_consulta;
         this.valor_consulta = valor_consulta;
         this.comparecimento_consulta = comparecimento_consulta;
+        this.horario = horario;
     }
-
     public Medico getMedico() { return medico; }
     public Paciente getPaciente() { return paciente; }
     public Agenda getAgenda() { return agenda_consulta; }
     public double getValor() { return valor_consulta; }
     public boolean getComparecimento_Consulta() {return comparecimento_consulta; }
-}
+    public Object getHorario() {return horario;};
+    }
