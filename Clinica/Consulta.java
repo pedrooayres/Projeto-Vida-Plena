42654db4566a3f4faa602bc5e77b2c0d1bdb35fd
Diogo Carvalho <150797053+Dioguitin@users.noreply.github.com>
2025-11-06 17:03:12 -0300

    Update Consulta.java

--- Clinica/Consulta.java
+++ Clinica/Consulta.java
@@ -1,7 +1,5 @@
 package Clinica;
 
-import java.time.LocalDateTime;
-
 import Base.Agenda;
 
 public class Consulta {
@@ -10,20 +8,18 @@ public class Consulta {
     public Agenda agenda_consulta;
     private double valor_consulta;
     public boolean comparecimento_consulta;
-    public LocalDateTime horario;
 
-    public Consulta(Paciente paciente, Medico medico, Agenda agenda_consulta, double valor_consulta, boolean comparecimento_consulta,LocalDateTime horario) {
+    public Consulta(Paciente paciente, Medico medico, Agenda agenda_consulta, double valor_consulta, boolean comparecimento_consulta) {
         this.paciente = paciente;
         this.medico = medico;
         this.agenda_consulta = agenda_consulta;
         this.valor_consulta = valor_consulta;
         this.comparecimento_consulta = comparecimento_consulta;
-        this.horario = horario;
     }
+
     public Medico getMedico() { return medico; }
     public Paciente getPaciente() { return paciente; }
     public Agenda getAgenda() { return agenda_consulta; }
     public double getValor() { return valor_consulta; }
     public boolean getComparecimento_Consulta() {return comparecimento_consulta; }
-    public Object getHorario() {return horario;};
-    }
+}
